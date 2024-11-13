50c596f211685acbd2e2daa8aae0d72bf719bbd1
Nikhil Verma <devnickverma@gmail.com>
2024-11-13 22:40:55 +0530

    Linked list

--- Linked list (Part 1)/LinkedList.java
+++ Linked list (Part 1)/LinkedList.java
@@ -1,299 +1,298 @@
-public class LinkedList{
+public class LinkedList {
 
-      public static class Node{
-           int data;
-           Node next;
-           
-           public Node(int data){
-             this.data = data;
-             this.next = null;
-           }
-       }
+    // Node class to represent each element in the linked list
+    public static class Node {
+        int data;
+        Node next;
 
-      public static Node head;
-      public static Node tail;
-      public static int size;
+        // Constructor to initialize node with data and null next pointer
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
 
-      public void addFirst(int data){
+    public static Node head; // Points to the first node of the list
+    public static Node tail; // Points to the last node of the list
+    public static int size;  // Stores the size of the linked list
 
-          // Step 1 :- create new node
-          Node newNode = new Node(data);
-          
-          size++;
+    // Method to add a node at the beginning of the list
+    public void addFirst(int data) {
+        // Step 1: Create a new node with the given data
+        Node newNode = new Node(data);
+        size++; // Increment the size
 
-          if(head == null){
-              head = tail = newNode;
-              return;
-          }
-
-          // Step 2 :- newNode next = head
-          newNode.next = head;
-
-          // Step 3 :- head = newNode
-          head = newNode;
-
-      }
-
-      public void addLast(int data){
-
-          // Step 1 :- create new node
-          Node newNode = new Node(data);
-
-          size++;
-
-          if(head==null){
+        // If the list is empty, set head and tail to the new node
+        if (head == null) {
             head = tail = newNode;
             return;
-          }
-
-          // Step 2 :- tail next = newNode
-          tail.next = newNode;
-
-          // Step 3:- tail = newNode
-          tail = newNode;
-
-      }
-
-      public void print(){
-        if(head == null){
-             System.out.print("Linked List is empty");
         }
+
+        // Step 2: Point newNode's next to the current head
+        newNode.next = head;
+
+        // Step 3: Move head to the newNode
+        head = newNode;
+    }
+
+    // Method to add a node at the end of the list
+    public void addLast(int data) {
+        // Step 1: Create a new node with the given data
+        Node newNode = new Node(data);
+        size++; // Increment the size
+
+        // If the list is empty, set head and tail to the new node
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+
+        // Step 2: Point tail's next to the newNode
+        tail.next = newNode;
+
+        // Step 3: Move tail to the newNode
+        tail = newNode;
+    }
+
+    // Method to print the linked list
+    public void print() {
+        if (head == null) {
+            System.out.print("Linked List is empty");
+            return;
+        }
+
         Node temp = head;
-        while(temp != null){
-             System.out.print(temp.data + " -> ");
-             temp = temp.next;
+        // Traverse and print each node's data
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
         }
         System.out.println("null");
-      }
+    }
 
-      public void add(int idx, int data){
- 
-        if(idx == 0){
-          addFirst(data);
-          return;
+    // Method to add a node at a specific index
+    public void add(int idx, int data) {
+        if (idx == 0) { // If index is 0, add at the beginning
+            addFirst(data);
+            return;
         }
 
         Node newNode = new Node(data);
         size++;
         Node temp = head;
         int i = 0;
-        
-        while(i < idx - 1){
-          temp = temp.next;
-          i++;
+
+        // Traverse to the (idx-1)th node
+        while (i < idx - 1) {
+            temp = temp.next;
+            i++;
         }
 
-        // i = idx - 1, temp -> prev
+        // Insert newNode between temp and temp.next
         newNode.next = temp.next;
         temp.next = newNode;
+    }
 
-      }
-
-      public int removeFirst(){
-
-        if(size == 0){
-          System.out.println("Linked list is empty");
-          return Integer.MIN_VALUE;
-        }
-        else if (size == 1){
-             int val = head.data;
-             head = tail = null;
-             size = 0;
+    // Method to remove the first node
+    public int removeFirst() {
+        if (size == 0) { // If list is empty, return error value
+            System.out.println("Linked list is empty");
+            return Integer.MIN_VALUE;
+        } else if (size == 1) { // If only one node, reset head and tail
+            int val = head.data;
+            head = tail = null;
+            size = 0;
             return val;
         }
 
-        int val = head.data;
-        head = head.next;
-        size--;
-        return val;    
-      }
-
-      public int removeLast(){
-        if(size == 0){
-          System.out.println("Linked list is empty");
-          return Integer.MIN_VALUE;
-        }
-        else if (size == 1){
-             int val = head.data;
-             head = tail = null;
-             size = 0;
-            return val;
-        }
-
-        // Prev : i = size - 2
-        Node prev = head;
-        for(int i = 0; i< size - 2; i++){
-             prev = prev.next;
-        } 
-        int val = prev.next.data; // tail.data
-        prev.next = null;
-        tail = prev;
+        int val = head.data; // Store data of the node to be removed
+        head = head.next; // Move head to the next node
         size--;
         return val;
+    }
 
-      }
-
-      public int itrSearch(int key){
-         Node temp = head;
-         int i = 0;
-
-         while(temp != null){
-             if(temp.data == key){
-               return i;
-             }
-             i++;
-             temp = temp.next;
-         }
-         
-         return -1;
-      }
-
-      
-      public int helper(Node head, int key){
-        if(head == null){
-          return -1;
+    // Method to remove the last node
+    public int removeLast() {
+        if (size == 0) { // If list is empty, return error value
+            System.out.println("Linked list is empty");
+            return Integer.MIN_VALUE;
+        } else if (size == 1) { // If only one node, reset head and tail
+            int val = head.data;
+            head = tail = null;
+            size = 0;
+            return val;
         }
 
-        if(head.data == key){
-          return 0;
+        // Traverse to the (size - 2)th node
+        Node prev = head;
+        for (int i = 0; i < size - 2; i++) {
+            prev = prev.next;
         }
-        int idx = helper(head.next, key);
-        if(idx == -1){
-          return -1;
+        int val = prev.next.data; // Store data of the last node
+        prev.next = null; // Remove link to the last node
+        tail = prev; // Update tail
+        size--;
+        return val;
+    }
+
+    // Method for iterative search of a key
+    public int itrSearch(int key) {
+        Node temp = head;
+        int i = 0;
+
+        // Traverse the list and check each node's data
+        while (temp != null) {
+            if (temp.data == key) {
+                return i; // Return index if key is found
+            }
+            i++;
+            temp = temp.next;
         }
 
-        return idx + 1;
-      }
+        return -1; // Return -1 if key is not found
+    }
 
-      public int resSearch(int key){
+    // Recursive helper method for search
+    public int helper(Node head, int key) {
+        if (head == null) {
+            return -1; // Base case: key not found
+        }
+
+        if (head.data == key) {
+            return 0; // Base case: key found at current node
+        }
+        int idx = helper(head.next, key); // Recursive search in the next node
+        return idx == -1 ? -1 : idx + 1; // Return index if key is found
+    }
+
+    // Public method to initiate recursive search
+    public int resSearch(int key) {
         return helper(head, key);
-      }
+    }
 
-      public void reverse(){
-             Node prev = null;
-             Node curr = tail = head;
-             Node next;
+    // Method to reverse the linked list
+    public void reverse() {
+        Node prev = null;
+        Node curr = tail = head;
+        Node next;
 
-             while(curr != null){
-              next = curr.next;
-              curr.next = prev;
-              prev = curr;
-              curr = next;
-             }
-             
-             head = prev;
-      }
- 
-      public void deleteNthfromEnd(int n){
-        // Calcculating size
+        // Traverse the list and reverse links
+        while (curr != null) {
+            next = curr.next; // Save the next node
+            curr.next = prev; // Reverse the link
+            prev = curr; // Move prev to current
+            curr = next; // Move to next node
+        }
+
+        head = prev; // Update head to the new front
+    }
+
+    // Method to delete the nth node from the end
+    public void deleteNthfromEnd(int n) {
+        // Calculate the size of the list
         int sz = 0;
         Node temp = head;
-        while(temp != null){
-          temp = temp.next;
-          sz++;
+        while (temp != null) {
+            temp = temp.next;
+            sz++;
         }
 
-        if(n == sz){
+        if (n == sz) { // If the nth node is the head
             head = head.next;
             return;
         }
 
-        // sz - n
-        int i = 1;
-        int iToFind = sz - n;
+        // Traverse to the (sz - n)th node
         Node prev = head;
-        while(i < iToFind){
-             prev = prev.next;
-             i++;
+        for (int i = 1; i < sz - n; i++) {
+            prev = prev.next;
         }
 
-        prev.next = prev.next.next;
-      }
+        prev.next = prev.next.next; // Remove the nth node from the end
+    }
 
-      // Slow fast approach
-      public Node findMid(Node head){
+    // Method to find the middle node using slow-fast approach
+    public Node findMid(Node head) {
         Node slow = head;
         Node fast = head;
 
-        while(fast != null && fast.next != null){
-           slow = slow.next; // +1
-           fast = fast.next.next; // +2
+        // Move fast by 2 steps and slow by 1 step
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
         }
-        return slow; // Slow is my mid
-      }
+        return slow; // Slow will be at the middle node
+    }
 
-      public boolean checkPalindrome(){
-
-        if(head == null || head.next == null){
-          return true;
+    // Method to check if the linked list is a palindrome
+    public boolean checkPalindrome() {
+        if (head == null || head.next == null) {
+            return true; // A single node or empty list is a palindrome
         }
 
-        // Step 1 :- find mid
+        // Step 1: Find the middle of the list
         Node midNode = findMid(head);
 
-        // Step 2 :- Reverse 2nd half
+        // Step 2: Reverse the second half of the list
         Node prev = null;
         Node curr = midNode;
         Node next;
 
-         while(curr != null){
-              next = curr.next;
-              curr.next = prev;
-              prev = curr;
-              curr = next;
-          }
-
-          Node right = prev; // Right half head
-          Node left = head;
-
-        // Step 3 :- Check left half and right half
-        while(right != null){
-          if(left.data != right.data){
-            return false;
-          }
-          left = left.next;
-          right = right.next;
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
         }
-          
-          return true;
-      }
 
-      public static void main(String args[]){
-          LinkedList ll = new LinkedList();
-          System.out.println();
-          ll.addFirst(4);
-          ll.addFirst(2);
-          ll.addFirst(1);
-          ll.addLast(5);
-          ll.add(2, 3);
-          ll.print();
-          
-          System.out.println("Size of linked list is : " + size); 
-          ll.removeFirst();
-          ll.print();
-          System.out.println("Size of linked list is : " + size);
-          ll.removeLast();
-          ll.print();
-          System.out.println("Size of linked list is : " + size); 
+        Node right = prev; // Right half head after reverse
+        Node left = head;
 
-          int key = 3;
-          int ans = ll.resSearch(key);
- 
-          if(ans != -1){
-               System.out.println(key + " is present at index : " + ans);
-          }
-          else {
-               System.out.println(key + " is not present in a linked list");
-          }
+        // Step 3: Check if left half matches the reversed right half
+        while (right != null) {
+            if (left.data != right.data) {
+                return false; // Not a palindrome
+            }
+            left = left.next;
+            right = right.next;
+        }
 
-          ll.reverse();
-          ll.print();
-          ll.deleteNthfromEnd(3);
-          ll.print();
-          ll.addLast(3);
-          ll.print();
-          System.out.print("Linked is palindrome : " + ll.checkPalindrome());
-  
-      }
+        return true; // List is a palindrome
+    }
 
+    public static void main(String args[]) {
+        LinkedList ll = new LinkedList();
+        
+        ll.addFirst(4);
+        ll.addFirst(2);
+        ll.addFirst(1);
+        ll.addLast(5);
+        ll.add(2, 3);
+        ll.print();
+        
+        System.out.println("Size of linked list is : " + size); 
+        ll.removeFirst();
+        ll.print();
+        System.out.println("Size of linked list is : " + size);
+        ll.removeLast();
+        ll.print();
+        System.out.println("Size of linked list is : " + size);
+
+        int key = 3;
+        int ans = ll.resSearch(key);
+
+        if (ans != -1) {
+            System.out.println(key + " is present at index : " + ans);
+        } else {
+            System.out.println(key + " is not present in a linked list");
+        }
+
+        ll.reverse();
+        ll.print();
+        ll.deleteNthfromEnd(3);
+        ll.print();
+        ll.addLast(3);
+        ll.print();
+        System.out.print("Linked list is palindrome: " + ll.checkPalindrome());
+    }
 }
