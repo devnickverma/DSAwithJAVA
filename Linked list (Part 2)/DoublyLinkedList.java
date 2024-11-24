cac64d4a6a63692d7c180e2cd7e0f384ad4be2e8
Nikhil Verma <devnickverma@gmail.com>
2024-11-24 23:05:18 +0530

    Doubly linked list

---
+++ Linked list (Part 2)/DoublyLinkedList.java
@@ -0,0 +1,102 @@
+public class DoublyLinkedList{
+    public class Node{
+        int data;
+        Node next;
+        Node prev;
+
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+    public static int size;
+    
+    // Add first
+    public void addFirst(int data){
+        Node newNode = new Node(data);
+        size++;
+        if(head == null){
+            head = tail = newNode;
+            return;
+        }
+        newNode.next = head;
+        head.prev = newNode;
+        head = newNode;
+    }
+
+    // Add last
+    public void addLast(int data){
+        Node newNode = new Node(data);
+        size++;
+        if(head == null){
+            head = tail = newNode;
+            return;
+        }
+        tail.next = newNode;
+        newNode.prev = tail;
+        tail = newNode;
+    }
+
+    // Remove first
+    public int removeFirst(){
+        if(size == 0){
+            System.out.println("List is empty");
+            return Integer.MIN_VALUE;
+        }else if(size == 1){
+            int val = head.data;
+            head = tail = null;
+            size = 0;
+            return val;
+        }
+        int val = head.data;
+        head = head.next;
+        head.prev = null;
+        size--;
+        return val;
+    }
+
+    // Remove last  
+    public int removeLast(){
+        if(size == 0){
+            System.out.println("List is empty");
+            return Integer.MIN_VALUE;
+        }else if(size == 1){
+            int val = head.data;
+            head = tail = null;
+            size = 0;
+            return val;
+        }
+        int val = tail.data;
+        tail = tail.prev;
+        tail.next = null;
+        size--;
+        return val;
+    }
+
+    public static void print(Node head){
+        Node temp = head;
+        while(temp != null){
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        DoublyLinkedList dll = new DoublyLinkedList();
+
+        dll.addFirst(10);
+        dll.addFirst(20);
+        dll.addLast(30);
+        dll.addLast(40);
+
+        dll.print(head);
+
+        System.out.println(dll.removeFirst());
+        System.out.println(dll.removeFirst());
+    }
+}
