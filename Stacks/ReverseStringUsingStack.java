1bbed4e949476e5c4dba52558732cfc39cead1e3
Nikhil Verma <devnickverma@gmail.com>
2024-11-26 22:51:27 +0530

    Stack

---
+++ Stacks/ReverseStringUsingStack.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class ReverseStringUsingStack{
+    public static String reverseString(String str){
+             Stack<Character> s = new Stack<>();
+             int idx = 0;
+             while(idx < str.length()){
+                s.push(str.charAt(idx));
+                idx++;
+             }
+             
+             StringBuilder sb = new StringBuilder("");
+             while(!s.isEmpty()){
+               sb.append(s.pop());
+             }
+             
+             return sb.toString();
+    }
+    public static void main(String args[]){
+        String str = "12345";
+        String revstr = reverseString(str);
+        System.out.println(revstr);  
+    }
+}
