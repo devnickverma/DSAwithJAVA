801927740666cd85c35c9e083c220339ef4b8307
Nikhil Verma <devnickverma@gmail.com>
2024-11-28 23:57:00 +0530

    Check For Valid Parentheses

--- Stacks/NextGreaterElement.java
+++ Stacks/NextGreaterElement.java
@@ -2,17 +2,25 @@ import java.util.*;
 public class NextGreaterElement{
     public static void nextGreaterElement(int arr[], int nextGreater[]){
            Stack<Integer> s = new Stack<>(); // stack to store next greater element
+           
            for(int i = arr.length - 1; i >= 0; i--){ // iterate from last element to first element
+               
+               // Step 1 - find next greater element
                while(!s.isEmpty() && s.peek() <= arr[i]){ // while stack is not empty and top of stack is less than current
                    s.pop(); // pop the top of stack as it is less than current element
                }
+
+               // Step 2 - store next greater element
                if(s.isEmpty()){ // if stack is empty 
                    nextGreater[i] = -1; // next greater element of current element is -1 as there is no element greater than current
                }else{
                    nextGreater[i] = s.peek(); // next greater element of current element is top of stack as there is an element greater than current
                }
+
+               // Step 3 - push current element in stack
                s.push(arr[i]); // push current element in stack as there is an element greater than current
            }
+           
     }
     public static void main(String args[]){
         int arr [] = {6, 8, 0, 1, 3};
---
+++ Stacks/ValidParentheses.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class ValidParentheses{
+    public static boolean isValid(String str) {
+        Stack<Character> s = new Stack<>();
+        for(int i = 0; i<str.length(); i++){
+            char ch = str.charAt(i);
+            // Push opening brackets
+            if(ch == '(' || ch == '{' || ch == '['){
+                s.push(ch);
+            }
+            // Pop closing brackets
+            else if(ch == ')' && !s.isEmpty() && s.peek() == '('){
+                s.pop();
+            }else if(ch == '}' && !s.isEmpty() && s.peek() == '{'){
+                s.pop();
+            }else if(ch == ']' && !s.isEmpty() && s.peek() == '['){
+                s.pop();
+            }else{
+                return false;
+            }
+        }
+        return s.isEmpty();
+    }
+    public static void main(String args[]){
+        String str = "()[]{}";
+        System.out.println("Parentheses are valid : " + isValid(str));
+    }
+}
